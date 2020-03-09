dabfd67f2b0d1e52bff9b8d80a5fcb3723dbf5be
jassi69 <jassi.khurana03@gmail.com>
2020-03-09 06:04:44 -0700

    improving code now

--- app/src/androidTest/java/com/imagedownloader/ExampleInstrumentedTest.kt
+++ app/src/androidTest/java/com/imagedownloader/ExampleInstrumentedTest.kt
@@ -1,12 +1,22 @@
 package com.imagedownloader
 
-import androidx.test.platform.app.InstrumentationRegistry
-import androidx.test.ext.junit.runners.AndroidJUnit4
 
+import android.view.inputmethod.EditorInfo.IME_ACTION_GO
+import androidx.test.espresso.Espresso.closeSoftKeyboard
+import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.action.ViewActions
+import androidx.test.espresso.action.ViewActions.typeText
+import androidx.test.espresso.matcher.ViewMatchers.*
+import androidx.test.espresso.web.sugar.Web.onWebView
+import androidx.test.ext.junit.rules.ActivityScenarioRule
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import androidx.test.platform.app.InstrumentationRegistry
+import com.imagedownloader.ui.MainActivity
+import org.junit.Assert.assertEquals
+import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
 
-import org.junit.Assert.*
 
 /**
  * Instrumented test, which will execute on an Android device.
@@ -15,10 +25,27 @@ import org.junit.Assert.*
  */
 @RunWith(AndroidJUnit4::class)
 class ExampleInstrumentedTest {
+
+    @get:Rule var activityScenarioRule = ActivityScenarioRule(MainActivity::class.java)
+
     @Test
     fun useAppContext() {
         // Context of the app under test.
         val appContext = InstrumentationRegistry.getInstrumentation().targetContext
         assertEquals("com.imagedownloader", appContext.packageName)
     }
+
+    @Test
+    fun openWebSite() {
+        onView(withId(R.id.etSearch)).perform(typeText("http://www.cricbuzz.com"))
+        onView(withId(R.id.etSearch)).perform(ViewActions.pressKey(IME_ACTION_GO))
+        closeSoftKeyboard()
+
+    }
+
+    @Test fun testWebViewInteraction() {
+        onWebView(withId(R.id.webVw)).forceJavascriptEnabled()
+      //  onWebView().check(webMatches(getText(), containsString("cricbuzz")))
+    }
+
 }
